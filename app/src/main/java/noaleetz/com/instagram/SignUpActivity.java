2d2bba605d4a99d5eeef1aa33933097755ad292b
Noa Schwartz <noaleetz@fb.com>
2018-07-13 18:58:25 -0700

    fixed signup bugs

--- app/src/main/java/noaleetz/com/instagram/SignUpActivity.java
+++ app/src/main/java/noaleetz/com/instagram/SignUpActivity.java
@@ -1,6 +1,8 @@
 package noaleetz.com.instagram;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,8 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
+
+import java.io.ByteArrayOutputStream;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -45,31 +51,50 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void signup(String username, String password,String email) {
-        ParseUser user = new ParseUser();
+        final ParseUser user = new ParseUser();
 // Set core properties
         user.setUsername(username);
         user.setPassword(password);
         user.setEmail(email);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_myperson);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
+        byte[] image = stream.toByteArray();
+        final ParseFile parseFile = new ParseFile(image);
+
+        parseFile.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                user.put("profilePic",parseFile);
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            // no errors!
+                            Log.d("LoginActivity","Sign Up was successful!");
+                            final Intent i = new Intent(SignUpActivity.this,HomeActivity.class);
+                            startActivity(i);
+                            finish();
+                        } else {
+                            // Sign up didn't succeed. Look at the ParseException
+                            // to figure out what went wrong
+                            Log.e("SignUpActivity","Sign Up Failed");
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+
+
+
 
         //user.setEmail("email@example.com");
 // Set custom properties
         //user.put("phone", "650-253-0000");
 // Invoke signUpInBackground
-        user.signUpInBackground(new SignUpCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    // no errors!
-                    Log.d("LoginActivity","Sign Up was successful!");
-                    final Intent i = new Intent(SignUpActivity.this,HomeActivity.class);
-                    startActivity(i);
-                    finish();
-                } else {
-                    // Sign up didn't succeed. Look at the ParseException
-                    // to figure out what went wrong
-                    Log.e("SignUpActivity","Sign Up Failed");
-                    e.printStackTrace();
-                }
-            }
-        });
+
     }
 }
